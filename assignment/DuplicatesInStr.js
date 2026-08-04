965bba91bd1275927cbb1731f8c4ce2f79e14b75
Vagdhevi <vagdhevigorli07@gmail.com>
2026-08-04 10:18:08 +0530

    Added Assignment Programs

--- assignment/DuplicatesInStr.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let str = "programming";
-let result = "";
-for (let char of str) {
-    if (!result.includes(char)) {
-        result = result + char;
-    }
-}
-console.log(result);
--- assignment/MergeArrays.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
-let merged = [...arr1, ...arr2];
-console.log(merged);
